24ec2e782bd3b91882902d9aab4c9268df14bdcb
Violet Szabó <V.Szabo-2@student.tudelft.nl>
2026-03-14 16:19:32 +0100

    Separate Langauge fike

---
+++ app/Language.hs
@@ -0,0 +1,17 @@
+module Language where
+
+import Data.Char
+import Data.Map
+
+data CharRange = CharRange { start :: Int, end :: Int } -- end excl
+
+singletonRange :: Char -> CharRange
+singletonRange c = CharRange { start = ord c, end = ord c + 1 } 
+
+data Production a = NonTerminal String a [String] | Terminal String CharRange
+
+data CFG a = CFG { prods :: Map String (Production a), startRule :: String }
+
+data Regex = One CharRange | Optional Regex | Star Regex | Plus Regex | Union Regex Regex | Concat Regex Regex
+
+data Budget = Unlimited | Finite Int
--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,6 @@
 module Main where
     
-import Data.Char
-import Data.Map
-
-data CharRange = CharRange { start :: Int, end :: Int } -- end excl
-
-singletonRange :: Char -> CharRange
-singletonRange c = CharRange { start = ord c, end = ord c + 1 } 
-
-data Production a = NonTerminal String a [String] | Terminal String CharRange
-
-data CFG a = CFG { prods :: Map String (Production a), startRule :: String }
-
-data Regex = One CharRange | Optional Regex | Star Regex | Plus Regex | Union Regex Regex | Concat Regex Regex
-
-data Budget = Unlimited | Finite Int
+import Language
 
 addrSpec :: Production Budget 
 addrSpec = NonTerminal "addr-spec" Unlimited ["local-part", "at-sign", "domain"]
