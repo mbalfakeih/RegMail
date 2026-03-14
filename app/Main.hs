a856889432806757891d8b59589d3be601212661
Violet Szabó <V.Szabo-2@student.tudelft.nl>
2026-03-14 15:53:41 +0100

    Budgets

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,13 @@ data CharRange = CharRange { start :: Int, end :: Int } -- end excl
 singletonRange :: Char -> CharRange
 singletonRange c = CharRange { start = ord c, end = ord c + 1 } 
 
-data Production = NonTerminal (String, [String]) | Terminal (String, CharRange)
+data Production a = NonTerminal (String, a, [String]) | Terminal (String, CharRange)
 
-data CFG = CFG { prods :: Map String Production, startRule :: String }
+data CFG a = CFG { prods :: Map String (Production a), startRule :: String }
 
 data Regex = One CharRange | Optional Regex | Star Regex | Plus Regex | Union Regex Regex | Concat Regex Regex
+
+data Budget = Unlimited | Finite Int
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
