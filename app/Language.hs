7433312d1d9c663e40ef047deb3ed89e9eb7316c
Violet Szabó <V.Szabo-2@student.tudelft.nl>
2026-03-14 16:26:36 +0100

    fix cfg def

--- app/Language.hs
+++ app/Language.hs
@@ -8,9 +8,9 @@ data CharRange = CharRange { start :: Int, end :: Int } -- end excl
 singletonRange :: Char -> CharRange
 singletonRange c = CharRange { start = ord c, end = ord c + 1 } 
 
-data Production a = NonTerminal String a [String] | Terminal String CharRange
+data Production = NonTerminal String [String] | Terminal String CharRange
 
-data CFG a = CFG { prods :: Map String (Production a), startRule :: String }
+data CFG = CFG { prods :: Map String [Production], startRule :: String, budgets :: Map String Budget }
 
 data Regex = One CharRange | Optional Regex | Star Regex | Plus Regex | Union Regex Regex | Concat Regex Regex
 
--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,10 @@ module Main where
     
 import Language
 
-addrSpec :: Production Budget 
-addrSpec = NonTerminal "addr-spec" Unlimited ["local-part", "at-sign", "domain"]
+addrSpec :: Production 
+addrSpec = NonTerminal "addr-spec" ["local-part", "at-sign", "domain"]
 
-atSign :: Production a
+atSign :: Production
 atSign = Terminal "at-sign" $ singletonRange '@'
 
 main :: IO ()
