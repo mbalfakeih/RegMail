5d4adeca5d3957c06afd21bae161a183b69faa8b
Violet Szabó <V.Szabo-2@student.tudelft.nl>
2026-03-14 15:45:46 +0100

    What is love

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
+    
+import Data.Char
+import Data.Map
+
+data CharRange = CharRange { start :: Int, end :: Int } -- end excl
+
+singletonRange :: Char -> CharRange
+singletonRange c = CharRange { start = ord c, end = ord c + 1 } 
+
+data Production = NonTerminal (String, [String]) | Terminal (String, CharRange)
+
+data CFG = CFG { prods :: Map String Production, startRule :: String }
+
+data Regex = One CharRange | Optional Regex | Star Regex | Plus Regex | Union Regex Regex | Concat Regex Regex
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
