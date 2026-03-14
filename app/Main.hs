2e55bc70b459de3f42cfe45f8cb78a586673cada
Violet Szabó <V.Szabo-2@student.tudelft.nl>
2026-03-14 21:58:18 +0100

    Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,19 @@ module Main where
     
 import Language
 import Grammar
+import Finitor (elaborateBudgets)
+import Data.Maybe
+import CfgToRegex
+import Data.Map.Lazy (assocs)
+
+
+getAllProds :: CFG -> [Production]
+getAllProds (CFG{prods = prods}) = concatMap snd $ assocs prods
 
 main :: IO ()
-main =  putStrLn (show $ checkConsistent productions)
+main = do
+    let cfg' = fromJust $ elaborateBudgets cfg
+    putStrLn ("Grammar length: " ++ (show $ length $ getAllProds cfg'))
+    print cfg'
+    let regex = cfgToRegex cfg'
+    print regex
